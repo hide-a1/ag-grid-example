ca52f40675d62614ebe50efd5c22e9560057dec9
hide-a1 <hide210g@gmail.com>
2024-05-19 09:56:04 +0900

    ng g c ag-grid-quick-st art

---
+++ src/app/ag-grid-quick-start/ag-grid-quick-start.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ag-grid-quick-start',
+  standalone: true,
+  imports: [],
+  templateUrl: './ag-grid-quick-start.component.html',
+  styleUrl: './ag-grid-quick-start.component.scss'
+})
+export class AgGridQuickStartComponent {
+
+}
