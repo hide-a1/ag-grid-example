b88b984fdefa860400debc53becb2605e49efbe4
hide-a1 <hide210g@gmail.com>
2024-05-19 10:15:19 +0900

    first angular grid
    https://www.ag-grid.com/angular-data-grid/getting-started/#your-first-angular-grid

--- src/app/ag-grid-quick-start/ag-grid-quick-start.component.ts
+++ src/app/ag-grid-quick-start/ag-grid-quick-start.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef } from 'ag-grid-community';
+
+interface IRow {
+  make: string;
+  model: string;
+  price: number;
+  electric: boolean;
+}
 
 @Component({
   selector: 'app-ag-grid-quick-start',
   standalone: true,
-  imports: [],
+  imports: [AgGridAngular],
   templateUrl: './ag-grid-quick-start.component.html',
-  styleUrl: './ag-grid-quick-start.component.scss'
+  styleUrl: './ag-grid-quick-start.component.scss',
 })
 export class AgGridQuickStartComponent {
+  themeClass = 'ag-theme-quartz';
 
+  // Row Data: The data to be displayed.
+  rowData: IRow[] = [
+    { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
+    { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
+  ];
+
+  // Column Definitions: Defines the columns to be displayed.
+  colDefs: ColDef<IRow>[] = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
+    { field: 'electric' },
+  ];
+
+  defaultColDef: ColDef = {
+    flex: 1,
+  };
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AgGridQuickStartComponent } from './ag-grid-quick-start/ag-grid-quick-start.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AgGridQuickStartComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ag-grid-example';
