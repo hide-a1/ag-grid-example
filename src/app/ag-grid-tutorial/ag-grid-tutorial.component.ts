cc115ec594de8699df8a1f4298f50397e130c536
hide-a1 <hide210g@gmail.com>
2024-05-19 15:22:35 +0900

    ng g c ag-grid-tutorial

---
+++ src/app/ag-grid-tutorial/ag-grid-tutorial.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ag-grid-tutorial',
+  standalone: true,
+  imports: [],
+  templateUrl: './ag-grid-tutorial.component.html',
+  styleUrl: './ag-grid-tutorial.component.scss'
+})
+export class AgGridTutorialComponent {
+
+}
