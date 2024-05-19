32cccd6fa1ea1b3fda87fd4923fce4450c19090e
hide-a1 <hide210g@gmail.com>
2024-05-19 15:41:30 +0900

    load new data
    https://www.ag-grid.com/angular-data-grid/deep-dive/#load-new-data

--- src/app/ag-grid-tutorial/ag-grid-tutorial.component.ts
+++ src/app/ag-grid-tutorial/ag-grid-tutorial.component.ts
@@ -1,12 +1,52 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef, GridReadyEvent } from 'ag-grid-community';
+import { lastValueFrom } from 'rxjs';
+
+interface IRow {
+  mission: string;
+  company: string;
+  location: string;
+  date: string;
+  time: string;
+  rocket: string;
+  price: number;
+  successful: boolean;
+}
 
 @Component({
   selector: 'app-ag-grid-tutorial',
   standalone: true,
-  imports: [],
+  imports: [AgGridAngular, HttpClientModule],
   templateUrl: './ag-grid-tutorial.component.html',
-  styleUrl: './ag-grid-tutorial.component.scss'
+  styleUrl: './ag-grid-tutorial.component.scss',
 })
 export class AgGridTutorialComponent {
+  themeClass = 'ag-theme-quartz';
+  // Row Data: The data to be displayed.
+  rowData: IRow[] = [];
 
+  // Column Definitions: Defines & controls grid columns.
+  colDefs: ColDef[] = [
+    { field: 'mission' },
+    { field: 'company' },
+    { field: 'location' },
+    { field: 'date' },
+    { field: 'price' },
+    { field: 'successful' },
+    { field: 'rocket' },
+  ];
+
+  // Load data into grid when ready
+  constructor(private http: HttpClient) {}
+
+  async onGridReady(params: GridReadyEvent) {
+    const res = await lastValueFrom(
+      this.http.get<any[]>(
+        'https://www.ag-grid.com/example-assets/space-mission-data.json'
+      )
+    );
+    this.rowData = res;
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AgGridQuickStartComponent } from './ag-grid-quick-start/ag-grid-quick-start.component';
+import { AgGridTutorialComponent } from './ag-grid-tutorial/ag-grid-tutorial.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AgGridQuickStartComponent],
+  imports: [RouterOutlet, AgGridTutorialComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
